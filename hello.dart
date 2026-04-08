fa9c15621a9aeb2ff62548642f458582e2163498
shadat0 <shadat0607@gmai.com>
2026-04-08 20:48:52 +0400

    added list name

--- hello.dart
+++ hello.dart
@@ -1,3 +1,7 @@
-void main(){
-    print("Hello World!");
+void main() {
+  print("Hello World!");
+  print("Hello Git!");
+
+  List<String> names = ["John", "Doe", "Jane", "Doe"];
+  print(names);
 }
