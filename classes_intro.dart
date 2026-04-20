4ae6709847c96cc8e139c436eb8cb87792446eb2
shadat0 <shadat0607@gmai.com>
2026-04-20 21:10:52 +0400

    declaring basic dart class, constructor and class methods

---
+++ classes_intro.dart
@@ -0,0 +1,39 @@
+void main() {
+  //create class
+
+  Person p1 = Person("Jhon", "Male", 44);
+  Person p2 = Person("Nafis", "Male", 55);
+  Person p3 = Person("Mary", "Female", 22);
+
+  // print(p1.age);
+  // p1.showperson();
+  // p2.showperson();
+  p3.showperson();
+  p3.intro();
+}
+
+class Person {
+  String? name, gender;
+  int? age;
+
+  //constructor
+
+  Person(String name, gender, int age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  //method
+  void showData() => print('''
+    Name = $name;
+    Gender = $gender;
+    Age = $age
+    
+    ''');
+
+  void showperson() => print(
+      "The name of this person is $name and is $gender and $age  years old");
+  void intro() => print(
+      "Hey everyone, Meet My Friend $name and he is $gender and he is $age years old");
+}
